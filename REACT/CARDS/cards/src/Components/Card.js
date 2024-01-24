9c4bb0dbe7381560e6c8a5902b6087e3d5185e1e
morit <moritz.brandner@student.htldornbirn.at>
2024-01-24 13:14:29 +0100

    Cards setup

--- REACT/CARDS/cards/src/Components/Card.js
+++ REACT/CARDS/cards/src/Components/Card.js
@@ -2,13 +2,13 @@ import React from "react";
 
 export default function Card({ name, title, imageUrl }) {
     return (
-        <div>
+        <div className="wrapper">
             <div>
                 <h2>{name}</h2>
                 <h2>{title}</h2>
             </div>
             <div>
-                <img src={imageUrl} alt=""></img>
+                <img src={imageUrl} alt="" className="image"></img>
             </div>
         </div>
     )
--- REACT/CARDS/cards/src/Components/PeopleContainers.js
+++ REACT/CARDS/cards/src/Components/PeopleContainers.js
@@ -4,10 +4,11 @@ import Card from "./Card.js";
 
 export default function PeopleContainer() {
     return (
-        <div clas>
+        <div>
             <h1 className="container">PeopleContainer</h1>
-
-            <Card name="hans" title="ceo" imageUrl="https://picsum.photos/200/300"></Card>
+            <div>
+                <Card name="hans" title="ceo" imageUrl="https://picsum.photos/200/300"></Card>
+            </div>
         </div>
     )
 }
