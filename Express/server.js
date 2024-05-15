a2703b2183f0f5d892207b1d0e0512a3366b086d
morit <moritz.brandner@student.htldornbirn.at>
2024-05-15 10:25:00 +0200

    node express 2 post done

--- Express/server.js
+++ Express/server.js
@@ -1,7 +1,10 @@
-const express = require("express")
+const express = require("express");
+const bodyParser = require("body-parser");
 
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 const port = 3000;
 
 const data = [{ name: "rudolf", age: 7 }, { name: "paul", age: 127 }, { name: "franz", age: 117 }, { name: "erik", age: 27 },]
@@ -10,6 +13,10 @@ app.get("/people", (req, res) => {
     res.send(data);
 });
 
+app.post("/people", (req, res) => {
+    data.push(req.body);
+    res.send(req.body);
+})
 
 app.listen(port, () => {
     console.log("Hello world app listening on port" + port)
